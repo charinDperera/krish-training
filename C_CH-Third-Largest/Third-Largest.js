2a2204c37e2df40387ff9bf9cf4511956648c495
Charin <kcdperera50000@gmail.com>
2022-03-09 13:48:24 +0530

    Commit of fix sorter Third Largest

--- C_CH-Third-Largest/Third-Largest.js
+++ C_CH-Third-Largest/Third-Largest.js
@@ -1,7 +1,7 @@
 function sorter(array){
-  for(i = 0;i < array.length; i++){
-    for(j = 0;j < array.length - i; j++){
-      if (array[j] > array[j+1]){
+  for(i = 0;i < array.length - 1; i++){
+    for(j = 0;j < array.length - i - 1; j++){
+	  if (array[j] > array[j+1]){
         temp = array[j];
         array[j] = array[j+1];
         array[j+1] = temp;
@@ -20,13 +20,14 @@ function main(){
         break;
       array.push(input);
     }
-    if (array.length > 3)
+    if (array.length >= 3)
       break;
     else
       console.log("Not enough Numbers in the series please enter more");
   }
   console.log(array)
-
+	if(3>20)
+		console.log("Balls")
   array = sorter(array);
   console.log(array)
   console.log("The third largest number is " + array[array.length - 3]);
