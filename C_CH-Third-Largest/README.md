27bca0d16e41a0689af573f91f5aaf79440cee12
Charin <kcdperera50000@gmail.com>
2022-03-09 13:10:44 +0530

    Commit of Coding Challenges Third Largest Solution

---
+++ C_CH-Third-Largest/README.md
@@ -0,0 +1,5 @@
+C_CH-Third-Largest
+
+Will get a series of integer user input, and will print the third largest number in that series.
+
+Approach: First we will sort the series in ascending order, and print the third element starting from the right side.
---
+++ C_CH-Third-Largest/Third-Largest.js
@@ -0,0 +1,37 @@
+function sorter(array){
+  for(i = 0;i < array.length; i++){
+    for(j = 0;j < array.length - i; j++){
+      if (array[j] > array[j+1]){
+        temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }    
+    }
+  }
+  return array;
+}
+
+function main(){
+  array = [];
+  while(true){
+    while(true){
+      const input = prompt("Enter numbers for the series, 0 to stop entering: ");
+      if(input == 0)
+        break;
+      array.push(input);
+    }
+    if (array.length > 3)
+      break;
+    else
+      console.log("Not enough Numbers in the series please enter more");
+  }
+  console.log(array)
+
+  array = sorter(array);
+  console.log(array)
+  console.log("The third largest number is " + array[array.length - 3]);
+}
+
+const prompt = require("prompt-sync")();
+
+main();
